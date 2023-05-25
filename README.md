19617033f9984e1aad305ff52eaee50b7e978ecd
Osvaldo Trevino <svldtrvn58@gmail.com>
2023-05-25 15:15:39 -0500

    Solution: Cat years, Dog years

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 1. Get the mean of an array [https://www.codewars.com/kata/563e320cee5dddcf77000158]
 2. Powers of 2 [https://www.codewars.com/kata/57a083a57cb1f31db7000028]
 3. All Star Code Challenge [https://www.codewars.com/kata/5865918c6b569962950002a1]
+4. Cat years, Dog years [https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b]
 
--- codewars.js
+++ codewars.js
@@ -1,10 +1,10 @@
-// Get the mean of an array
+// 1. Get the mean of an array
 
 function getAverage(marks) {
   return Math.floor(marks.reduce((acc, c) => acc + c, 0) / marks.length);
 }
 
-// Powers of 2
+// 2. Powers of 2
 
 function powersOfTwo(n) {
   //   For Loop Method#1
@@ -21,7 +21,15 @@ function powersOfTwo(n) {
   //   return Array.from({length: n+1}, (v, i) => 2**i)
 }
 
-// All Star Code Challenge
+// 3. All Star Code Challenge
 function strCount(str, letter) {
   return str.split('').filter((l) => l === letter).length;
 }
+
+// 4. Cat years, Dog years
+
+var humanYearsCatYearsDogYears = function (y) {
+  if (y == 1) return [1, 15, 15];
+  if (y == 2) return [2, 24, 24];
+  return [y, (y - 2) * 4 + 24, (y - 2) * 5 + 24];
+};
